70b482d3f143342d0dd2f52dcd8680661f51ee86
Jett Penner <jettski12@gmail.com>
2023-04-04 09:06:17 -0600

    Auto-generated commit [ci skip]

--- src/app/project/project.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
-})
-export class ProjectComponent {}
